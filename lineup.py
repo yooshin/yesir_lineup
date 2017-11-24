9b1da01012ef9f6dd11dc4af559fa894d2e5bd97
YooshinKang-PC\Yooshin Kang <yooshinkang@gmail.com>
2017-11-24 18:50:47 +0900

    hello world를 통한 TKinter Package 실습

--- lineup.py
+++ lineup.py
@@ -10,9 +10,25 @@ Created on 2017-11-24
 import panadas
 from tkinter import *
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
 
-def window
-    """
-    Returns : 
-    """
-    window = Tk()
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
