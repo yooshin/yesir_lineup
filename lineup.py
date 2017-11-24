894c9c0890a56deb0435080fb3cee90905ffc5e0
YooshinKang-PC\Yooshin Kang <yooshinkang@gmail.com>
2017-11-24 18:36:24 +0900

    최초 코드 작성

---
+++ lineup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-11-24
+
+@author: 강유신
+"""
+
+
+
+import panadas
+from tkinter import *
+
+
+def window
+    """
+    Returns : 
+    """
+    window = Tk()
